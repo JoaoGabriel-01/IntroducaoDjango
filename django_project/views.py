4e0c745f8bb5473d57c23b2ddf6d1a6877491f1f
João Gabriel <106453811+JoaoGabriel-01@users.noreply.github.com>
2022-06-10 14:09:34 +0000

    Trabalhado com If/Elif/Else/Endif e colocando a hora local

--- django_project/views.py
+++ django_project/views.py
@@ -1,8 +1,10 @@
 #from django.http import HttpResponse
 from django.shortcuts import render
+from datetime import datetime
 
 def greeting(request):
   context = {
-    'greeting' : 'Olá! Este é um exemplo de uso de templates. :)'
+    'greeting' : 'Olá! Este é um exemplo de uso de templates. :)',
+    'today' : datetime.now()
   }
   return render(request, 'greeting.html', context)
